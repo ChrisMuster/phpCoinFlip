98157ce3c0ef43771b4784bd0dac002752fc85c3
Chris Muster <christophermuster@yahoo.co.uk>
2017-03-27 14:43:42 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Coin Flip</title>
+			<meta charset="utf-8"> 
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" type="text/css" href="styles/main.css">
+	</head>
+	<body>
+		<div class="main">
+			<h1>Coin Flip!</h1>
+			<p>How many flips will it take to get 3 Heads in a row?</p>
+			<div class="cointable">
+				<?php
+					$headCount = 0;
+					$flipCount = 0;
+					while ($headCount < 3) {
+						$flip = rand(0,1);
+						$flipCount ++;
+						if ($flip){
+							$headCount ++;
+							echo "<div class=\"coin\" id=\"H\">H</div>";
+						}
+						else {
+							$headCount = 0;
+							echo "<div class=\"coin\" id=\"T\">T</div>";
+						}
+					}
+					echo "<p>It took {$flipCount} flips!</p>";	
+				?>
+			</div>
+			<h2>Reload page to try again!</h2>
+		</div>
+	</body>
+</html>
