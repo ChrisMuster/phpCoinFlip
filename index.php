31d476550b0cc8beeced984ae4ecfc7b0ba1d493
Chris Muster <christophermuster@yahoo.co.uk>
2017-03-27 14:45:48 +0100

    Create index.php for Coin Flip

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<title>Coin Flip</title>
 			<meta charset="utf-8"> 
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" type="text/css" href="styles/main.css">
+			<link rel="stylesheet" type="text/css" href="main.css">
 	</head>
 	<body>
 		<div class="main">
