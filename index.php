ad41ffcfa8bde5971270add09bc7efff291f345e
Chris Muster <christophermuster@yahoo.co.uk>
2020-05-28 14:39:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,40 +4,24 @@
 		<title>Coin Flip</title>
 			<meta charset="utf-8"> 
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" type="text/css" href="styles/main.css">
+			<link rel="stylesheet" type="text/css" href="styles/coinflip.css">
 	</head>
 	<body>
 		<div class="main">
+
 			<h1>Coin Flip!</h1>
+
 			<p>How many flips will it take to get 3 Heads in a row?</p>
-			<div class="cointable">
-				<?php
-					$headCount = 0;
-					$flipCount = 0;
-					while ($headCount < 3) {
-						$flip = rand(0,1);
-						$flipCount ++;
-						if ($flip){
-							$headCount ++;
-							echo '<div class="coin" id="H">H</div>';
-						}
-						else {
-							$headCount = 0;
-							echo '<div class="coin" id="T">T</div>';
-						}
-					}
-					echo "<p>It took {$flipCount} flips!</p>";	
-				?>
-			</div>
-			<button onclick="myReload()" class="flip">
+
+			<div class="cointable"><h3>Click below to begin</h3></div>
+
+			<button id="flipCoin" class="flip">
 				<h2>Flip again!</h2>
 			</button>
+
 		</div>
 
-		<script>
-			function myReload() {
-			    location.reload(true);
-			}
-		</script>	
+		<script src="js/jquery-3.5.1.min.js"></script>
+		<script src="js/coinflip.js"></script>
 	</body>
 </html>
