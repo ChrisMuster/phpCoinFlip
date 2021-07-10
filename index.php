3e794f6157fcb06b2d52216ccc3ffabc0591f513
Chris Muster <christophermuster@yahoo.co.uk>
2021-07-10 16:39:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
 		<title>Coin Flip</title>
 			<meta charset="utf-8"> 
 			<meta name="viewport" content="width=device-width, initial-scale=1">
-			<link rel="stylesheet" type="text/css" href="styles/coinflip.css">
+			<link rel="stylesheet" type="text/css" href="styles/coinflip.css?rnd=132">
 	</head>
 	<body>
 		<div class="main">
 
 			<h1>Coin Flip!</h1>
+			
+			<p>When you click the Flip button, the computer will begin flipping coins and recording the results.</p>
+			<p>It will continue until it has 3 Heads in a row, then display the full results.</p>
 
 			<p>How many flips will it take to get 3 Heads in a row?</p>
 
