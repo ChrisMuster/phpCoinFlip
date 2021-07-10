956bb467cdabc1b2f143225699cc0288b8816c1d
Chris Muster <christophermuster@yahoo.co.uk>
2021-07-10 16:37:48 +0100

    Update coinflip.php

--- coinflip.php
+++ coinflip.php
@@ -7,11 +7,11 @@
 		$flipCount ++;
 		if ($flip){
 			$headCount ++;
-			$coins .= '<div class="coin H">H</div>';
+			$coins .= '<div class="coin heads">H</div>';
 		}
 		else {
 			$headCount = 0;
-			$coins .= '<div class="coin T">T</div>';
+			$coins .= '<div class="coin tails">T</div>';
 		}
 	}
 
